cee73e8df09d67d4f576809ffff6701791c5e7c6
Enrico Sorcinelli <enrico.sorcinelli@italiaonline.it>
2019-06-17 17:00:59 +0200

    Add support for ACF; added translations

--- php/class-seo-writing-assistant-semrush-custom-fields.php
+++ php/class-seo-writing-assistant-semrush-custom-fields.php
@@ -25,11 +25,18 @@ class SEO_Writing_Assistant_SEMrush_Custom_Fields {
 	private $settings = array();
 
 	/**
+	 * Plugin settings.
+	 *
+	 * @var array
+	 */
+	private $plugin_settings;
+
+	/**
 	 * Prefix used for options and postmeta fields, DOM IDs and DB tables.
 	 *
 	 * @var string
 	 */
-	private static $prefix = 'swa_semrush_custom_fields_plugin_';
+	private static $prefix = 'swa_semrush_custom_fields_';
 
 	/**
 	 * Instance of this class.
@@ -57,6 +64,9 @@ class SEO_Writing_Assistant_SEMrush_Custom_Fields {
 			),
 			$args
 		);
+
+		// Load plugin text domain.
+		load_plugin_textdomain( 'swa-semrush-custom-fields', false, dirname( plugin_basename( __FILE__ ) ) . '/../languages/' );
 
 		// Check and load needed compoments.
 		if ( is_admin() ) {
@@ -94,10 +104,23 @@ class SEO_Writing_Assistant_SEMrush_Custom_Fields {
 			return;
 		}
 
+		// Get plugin settings.
+		$this->plugin_settings = $this->get_plugin_default_settings();
+
 		// Add JS to post pages.
 		if ( ! self::is_ajax_request() ) {
 			add_action( 'admin_print_scripts-post.php', array( $this, 'load_javascript' ), 10, 0 );
 			add_action( 'admin_print_scripts-post-new.php', array( $this, 'load_javascript' ), 10, 0 );
+		}
+
+		// Check for ACF.
+		if ( function_exists( 'get_field' ) ) {
+			add_action( 'acf/render_field_settings/type=text', array( $this, 'acf_render_field_settings' ) );
+			add_action( 'acf/render_field_settings/type=textarea', array( $this, 'acf_render_field_settings' ) );
+			add_action( 'acf/render_field_settings/type=select', array( $this, 'acf_render_field_settings' ) );
+			add_action( 'acf/render_field_settings/type=radio', array( $this, 'acf_render_field_settings' ) );
+			add_action( 'acf/render_field_settings/type=checkbox', array( $this, 'acf_render_field_settings' ) );
+			add_filter( 'acf/prepare_field', array( $this, 'acf_prepare_field' ) );
 		}
 	}
 
@@ -123,6 +146,7 @@ class SEO_Writing_Assistant_SEMrush_Custom_Fields {
 			return;
 		}
 
+		// Add JavaScript.
 		wp_enqueue_script(
 			self::$prefix . 'js',
 			SWA_SEMRUSH_CUSTOM_FIELDS_PLUGIN_BASEURL . '/assets/js/swa-semrush-custom-fields.js',
@@ -130,6 +154,49 @@ class SEO_Writing_Assistant_SEMrush_Custom_Fields {
 			SWA_SEMRUSH_CUSTOM_FIELDS_PLUGIN_VERSION,
 			false
 		);
+
+		// Localization.
+		wp_localize_script(
+			self::$prefix . 'js',
+			self::$prefix . 'i18n',
+			array(
+				'enable'   => $this->plugin_settings['enable'],
+				'interval' => $this->plugin_settings['interval'],
+			)
+		);
+	}
+
+	/**
+	 * Add custom ACF settings.
+	 *
+	 * @param array $field Field data.
+	 */
+	function acf_render_field_settings( $field ) {
+
+		acf_render_field_setting(
+			$field,
+			array(
+				'label'        => __( 'SEMrush', 'swa-semrush-custom-fields' ),
+				'instructions' => __( 'Add field value to the text used by SEMrush Writing Assistant analysis', 'swa-semrush-custom-fields' ),
+				'name'         => 'swa_scf',
+				'type'         => 'true_false',
+				'ui'           => 1,
+			)
+		);
+	}
+
+	/**
+	 * Add class to ACF fields.
+	 *
+	 * @param array $field Field data.
+	 *
+	 * @return mixed
+	 */
+	function acf_prepare_field( $field ) {
+		if ( ! empty( $field['swa_scf'] ) ) {
+			$field['class'] .= ' swa-scf';
+		}
+		return $field;
 	}
 
 	/**
@@ -151,6 +218,30 @@ class SEO_Writing_Assistant_SEMrush_Custom_Fields {
 	}
 
 	/**
+	 * Get plugin options settings.
+	 *
+	 * @return array
+	 */
+	private function get_plugin_default_settings() {
+
+		$settings = array(
+			'enable'   => ( defined( 'SWA_SEMRUSH_CUSTOM_FIELDS_PLUGIN_AUTOENABLE' ) && SWA_SEMRUSH_CUSTOM_FIELDS_PLUGIN_AUTOENABLE === true ) ? true : false,
+			'interval' => defined( 'SWA_SEMRUSH_CUSTOM_FIELDS_PLUGIN_INTERVAL' ) ? absint( SWA_SEMRUSH_CUSTOM_FIELDS_PLUGIN_INTERVAL ) : 5,
+		);
+
+		/**
+		 * Filter plugin settings.
+		 *
+		 * @param array $settings Plugin settings.
+		 *
+		 * @return array
+		 */
+		$settings = apply_filters( self::$prefix . 'settings', $settings );
+
+		return $settings;
+	}
+
+	/**
 	 * Plugin activation hook.
 	 *
 	 * @return void
@@ -162,11 +253,14 @@ class SEO_Writing_Assistant_SEMrush_Custom_Fields {
 		if ( current_user_can( 'activate_plugins' )
 			&& ! is_plugin_active( 'semrush-seo-writing-assistant/semrush-seo-writing-assistant.php' ) ) {
 
+			// Load translations.
+			load_plugin_textdomain( 'swa-semrush-custom-fields', false, dirname( plugin_basename( __FILE__ ) ) . '/../languages/' );
+
 			// Throw an error in the WordPress admin console.
 			die(
 				sprintf(
-					'<p style="color: #444; font-size: 13px; line-height: 1.4em;font-family: -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Oxygen-Sans, Ubuntu, Cantarell, \'Helvetica Neue\', sans-serif;">' . __( 'The <i>%1$s</i> plugin requires %2$s plugin to be active.', 'swa-semrush-custom-fields' ) . '</p>',
-					'SEO Writing Assistant SEMrush  Custom Fields',
+					/* translators: 1 current plugin name, 2 plugin dependency name */'<p style="color: #444; font-size: 13px; line-height: 1.4em;font-family: -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Oxygen-Sans, Ubuntu, Cantarell, \'Helvetica Neue\', sans-serif;">' . __( 'The %1$s plugin requires %2$s plugin to be active.', 'swa-semrush-custom-fields' ) . '</p>',
+					'<i>SEO Writing Assistant SEMrush  Custom Fields</i>',
 					'<a target="_blank" href="' . esc_url( 'https://wordpress.org/plugins/semrush-seo-writing-assistant/' ) . '">SEMrush SEO Writing Assistant</a>'
 				)
 			); // WPCS: XSS ok.
